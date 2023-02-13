d005433f38b7901c2816373105605a6fa8b9e26a
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-13 19:09:41 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println("");
+        double [] arrayTwoo = {1.57, 7.654, 9.986};
+        System.out.println(arrayTwoo[0]);
+        System.out.println(arrayTwoo[1]);
+        System.out.println(arrayTwoo[2]);
+        System.out.println("");
+        int[] arrayThree = {5, 8, 63, 46, 86, 563};
+                for (int i =0; i < arrayThree.length; i++){
+                    System.out.println(arrayThree [i]);
+                }
     }
 }

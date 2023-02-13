0903b4c3caa442834ad62343461d0ca8dcb2c991
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-13 19:52:29 +0300

    Задача №2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -13,14 +14,39 @@ public class Main {
         System.out.println(array[1]);
         System.out.println(array[2]);
         System.out.println("");
-        double [] arrayTwoo = {1.57, 7.654, 9.986};
+        double[] arrayTwoo = {1.57, 7.654, 9.986};
         System.out.println(arrayTwoo[0]);
         System.out.println(arrayTwoo[1]);
         System.out.println(arrayTwoo[2]);
         System.out.println("");
         int[] arrayThree = {5, 8, 63, 46, 86, 563};
-                for (int i =0; i < arrayThree.length; i++){
-                    System.out.println(arrayThree [i]);
-                }
+        for (int i = 0; i < arrayThree.length; i++) {
+            System.out.println(arrayThree[i]);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        System.out.print(array[0] + ", ");
+        System.out.print(array[1] + ", ");
+        System.out.print(array[2]);
+        System.out.println("");
+        double[] arrayTwoo = {1.57, 7.654, 9.986};
+        System.out.print(arrayTwoo[0] + ", ");
+        System.out.print(arrayTwoo[1] + ", ");
+        System.out.print(arrayTwoo[2]);
+        System.out.println("");
+        int[] arrayThree = {5, 8, 63, 46, 86, 563};
+        for (int i = 0; i < arrayThree.length; i++) {
+            if (i < arrayThree.length-1) {
+                System.out.print(arrayThree[i] + ", ");
+            } else {
+                System.out.print(arrayThree[i]);
+            }
+        }
     }
 }
